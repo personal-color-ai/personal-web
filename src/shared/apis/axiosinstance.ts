a5c50de0f91e041102ba65a646d44d9373381aab
choikyungsoo <ryan1954@naver.com>
2025-11-27 11:46:07 +0900

    Fix: Vercel 프록시 설정 추가

--- src/shared/apis/axiosinstance.ts
+++ src/shared/apis/axiosinstance.ts
@@ -1,6 +1,8 @@
-import axios from 'axios';
+import axios from "axios";
+
+const isProd = import.meta.env.PROD; // 배포 환경인지 확인
 
 export const instance = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL,
+  baseURL: isProd ? "/api" : import.meta.env.VITE_BACKEND_URL,
   withCredentials: true,
 });
